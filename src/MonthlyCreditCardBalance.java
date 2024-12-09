edb126b2ad7338501da5bb6145cb038ea94bbe9a
DavidM482 <martin.613901@swl.k12.oh.us>
2024-12-09 08:42:45 -0500

    change

--- src/MonthlyCreditCardBalance.java
+++ src/MonthlyCreditCardBalance.java
@@ -4,6 +4,12 @@ public class MonthlyCreditCardBalance {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         //variables
+        int cardBalance = 5000;
+        double interestRate = 0.17;
 
+        cardBalance += cardBalance * interestRate;
+        System.out.println("The balance of the card after one month is $" + cardBalance);
+        cardBalance += cardBalance * interestRate;
+        System.out.println("The balance of the card after two months is $" + cardBalance);
     }
 }
